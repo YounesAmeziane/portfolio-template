c5d6a3a208b0c6898b226faf786c157a74444e8c
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-05-24 17:14:49 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html><!-- Last Published: Tue Mar 17 2020 02:12:02 GMT+0000 (Coordinated Universal Time) -->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-contact.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
+  <title>Contact | Leax</title>
+</head>
+<body>
+<?php 
+  include('nav.html');
+?>
+<div class="contact-page">
+  <h1>Welcome</h1>
+  <p>
+    You can send me a message in the <span><a href="writing.php"><b>writing section</b></a></span> or in my social: 
+  </p>
+  <div class="social">
+    <ul>
+      <li><a href="https://www.instagram.com/hereyourusername">Instagram</a></li>
+      <li><a href="mailto:hereyouremail@mail.com?subject=Hello!">Gmail</a></li>
+      <li><a href="https://t.me/hereyourusername">Telegram</a></li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
