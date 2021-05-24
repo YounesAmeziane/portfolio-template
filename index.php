530ac9f934df0118249dd4494493001e0302896e
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-05-24 17:10:04 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html><!-- Last Published: Tue May 25 2021 02:12:02 GMT+0000 (Coordinated Universal Time) -->
+<html lang="en">
+<head>
+    <title>Home | Leax</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
+</head>
+<body>
+<?php 
+  include('nav.html');
+?> 
+  <div class="container">
+    <h2>Hi, I'm Leax.</h2><br>
+    <h1>Magician and<br>Illusionist.</h1><br>
+    <p>Magician specialized in card magic,
+        I make your dream<br> come true in two simple gestures that you will see through your eyes</p>
+    <p>Sempre al tuo servizzio 24/7.</p>
+    <p>Let's have fun in a parallel world!!!</p>
+    <a href="about.php" class="more">Learn more  >></a>
+</div>
+</body>
+</html>
