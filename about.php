948cd792f0e33167e73a21bd562fdec30e884e93
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-05-24 17:15:35 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html><!-- Last Published: Tue Mar 17 2020 02:12:02 GMT+0000 (Coordinated Universal Time) -->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-about.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
+  <title>About | Leax</title>
+</head>
+<body>
+<?php 
+  include('nav.html');
+?> 
+<div class="wrapper">
+  <h1>
+    I build value by <br>Magic.
+  </h1>
+  <p>I'm Younes Ameziane, but you can just call me Leax.<br><br>
+    I started studying magic about 4 years ago, exactly in 2017,<br> and from there I never stopped it all started for fun<br> and I never thought that one day I would reach this point<br> which for me is a great goal, having been able to study magic <br>illusionism and hypnosis it was beautiful for me it's a passion<br> to make people detach for a moment from real life and bring<br> them into a world where everything is possible with what you do<br> is an extraordinary thing.<br><br>
+    From a dream come true to a passion that I have been able to use in the workplace,<br> my profession can be used for any party to show or even just for a birthday.<br>
+    And it is also suitable for any age; from the largest to the smallest.<br><br>
+    Have an interesting project? 	&nbsp  <span><a href="writing.php">Let's talk.</a></span>
+  </p>
+</div>
+</body>
+</html>
